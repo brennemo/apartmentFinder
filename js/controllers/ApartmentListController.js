4e550aed9e09190792471c297f1b27101f0292f7
Morgan Brenner <brennemo@oregonstate.edu>
2017-04-12 11:49:55 -0400

    renamed variables

---
+++ js/controllers/ApartmentListController.js
@@ -0,0 +1,40 @@
+app.controller('ApartmentListController', function() {
+    var apartmentList = this;
+    apartmentList.apartments = [
+      {
+		text:'Apartment 1', 
+		done:true,
+		date:  new Date('2016', '08', '01'), 
+		photo: 'img/placeholder.png',
+		likes: 0
+	  },
+      {
+		text:'Apartment 2', 
+		done:false,
+		date:  new Date('2016', '08', '01'),
+		photo: 'img/placeholder.png',
+		likes: 0
+	  }
+	];
+ 
+    apartmentList.addApartment = function() {
+      apartmentList.apartments.push({text:apartmentList.apartmentText, done:false, date: new Date('2016', '08', '01'), photo: 'img/placeholder.png', likes:0});
+      apartmentList.apartmentText = '';
+    };
+ 
+    apartmentList.remaining = function() {
+      var count = 0;
+      angular.forEach(apartmentList.apartments, function(apartment) {
+        count += apartment.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    apartmentList.archive = function() {
+      var oldApartments = apartmentList.apartments;
+      apartmentList.apartments = [];
+      angular.forEach(oldApartments, function(apartment) {
+        if (!apartment.done) apartmentList.apartments.push(apartment);
+      });
+    };
+});
