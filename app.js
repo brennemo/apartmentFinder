2e68352f5c969394085d1f4295c91fba5bb7f151
Morgan Brenner <brennemo@oregonstate.edu>
2017-04-15 15:13:19 -0400

    moved views, css, client js into /public

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; 
+var bodyParser = require('body-parser');
 
+//MongoDB Schema 
+mongoose.connect('mongodb://localhost/test');
+
+var ApartmentSchema = new Schema({
+  name: String,
+  price: Number,
+  date_available: { type: Date, default: Date.now },
+});
+
+//Basic Hello World 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
