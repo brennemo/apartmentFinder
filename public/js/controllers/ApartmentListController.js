3f00a458a5cfa7e393bbf2fefa205f81a7d5beeb
Morgan Brenner <brennemo@oregonstate.edu>
2017-04-17 09:57:56 -0400

    added placeholders for services in controller

--- public/js/controllers/ApartmentListController.js
+++ public/js/controllers/ApartmentListController.js
@@ -11,8 +11,24 @@ angular.module('AppCtrl', []).controller('ApartmentListController', ['$scope', '
                 $scope.apartments = response.data;   
         }, function (error) {
                 $scope.status = 'Unable to load apartment data: ' + error.message;
-            }); 
-        
+            });  
     }
+    
+    $scope.getApartment() = function(apt_id) {
+        console.log("getApartment() will happen here.");    
+    }
+    
+    $scope.insertApartment() = function(apt_id) {
+        console.log("insertApartment() will happen here.");    
+    }
+    
+    $scope.updateApartment() = function(apt_id) {
+        console.log("updateApartment() will happen here.");    
+    }
+    
+    $scope.deleteApartment() = function(apt_id) {
+        console.log("deleteApartment() will happen here.");    
+    }
+    
     
 }]);
--- public/js/services/ApartmentListService.js
+++ public/js/services/ApartmentListService.js
@@ -7,11 +7,19 @@ angular.module('ApartmentListService', []).factory('ApartmentListService', ['$ht
         
         getApartment : function(apt_id) {
             return $http.get('/api/apartments' + apt_id);
-        }
+        },
         
-        insertApartment: function(apt) {
-            return $http.post('/api/apartments', apt);
+        insertApartment: function(apt_id) {
+            return $http.post('/api/apartments', apt_id);
+        },
+            
+        updateApartment: function(apt_id) {
+            return $http.put('/api/apartments' + apt_id, apt_id);
+        },
+        
+        deleteApartment: function(apt_id) {
+            return $http.delete('/api/apartments' + apt_id);
         }
-    }       
+    }      
 
 }]);
